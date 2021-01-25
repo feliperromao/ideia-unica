c016b9d0a00745b60980b10cccc411ffc5a82c48
Felipe <felipe.martins@fortics.com.br>
2021-01-24 21:40:56 -0300

    add logs

--- pages/tempo.js
+++ pages/tempo.js
@@ -1,4 +1,5 @@
 function Tempo(props) {
+    console.log('Passando pelo frontend')
     const dynanicDate = new Date()
     const dynanicDateString = dynanicDate.toGMTString()
 
@@ -12,6 +13,7 @@ function Tempo(props) {
 }
 
 export function getStaticProps() {
+    console.log('Passando pelo getStaticProps')
     const staticDate = new Date()
     const staticDateString = staticDate.toGMTString()
 
